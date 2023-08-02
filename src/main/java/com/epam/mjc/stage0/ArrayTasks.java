1b978d129e352ebc32fb9f64cc2e9ca2ebeb890c
MirrorwindAbyss <abdumalikunusov06@gmail.com>
2023-08-02 12:46:32 +0500

    Done!

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -127,8 +127,8 @@ public class ArrayTasks {
         for(int i=0; i<arr.length-1; i++){
             for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j].length>arr[j+1].length){
-                    int[] temp = arr[j-1];
-                    arr[j-1]=arr[j+1];
+                    int[] temp = arr[j];
+                    arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
             }
